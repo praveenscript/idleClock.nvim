bc9843f9d4260ce8d2465fb0d9659213591b778f
praveenscript <praveenkumarjkr@gmail.com>
2025-09-25 20:49:16 +0530

    initial commmit

---
+++ lua/idleClock/init.lua
@@ -0,0 +1,54 @@
+local M = {}
+
+
+function M.setup(opts)
+  opts = opts or {}
+
+  local async = require("plenary.async")
+  local floatingClock = require("idleClock.floatingClock")
+  local startTimer = require("idleClock.startTimer")
+
+
+  local idleTimeLimit = opts.idleTimeLimit or 30 -- time for the user idle start before logic begins
+  local idleWaiting = 0
+  local triggersCounts = 0
+
+
+  local function startWaiting(triggerCount)
+    async.run(function()
+      while triggersCounts == triggerCount do
+        async.util.sleep(6000) -- Every 6 seconds
+        idleWaiting = idleWaiting + 6
+
+        if triggerCount ~= triggersCounts then
+          idleWaiting = 0
+          break
+        end
+        if idleWaiting > idleTimeLimit then
+          floatingClock.createBuffer()
+          break
+        end
+      end
+    end)
+  end
+
+
+
+
+
+  -- Reset on any user activity
+  vim.api.nvim_create_autocmd({ "CursorMoved", "InsertEnter", "BufWritePost" }, {
+    callback = function()
+      triggersCounts = triggersCounts + 1 -- increment every 1 time
+      startWaiting(triggersCounts)
+      print("Yo LOl", triggersCounts)
+      startTimer.stop = true
+      if floatingClock.created then
+        floatingClock.close()
+        floatingClock.created = false
+      end
+    end,
+  })
+end
+
+return M
---
+++ lua/idleClock/startTimer.lua
@@ -0,0 +1,40 @@
+local async = require("plenary.async")
+
+local M = {}
+
+M.timeSpent = nil
+M.stop = false
+function M.run()
+
+
+
+
+local function format_time(seconds)
+    local mins = math.floor(seconds / 60)
+    local secs = seconds % 60
+    return string.format("%02d:%02d", mins, secs)
+end
+
+
+
+local function startTimer()
+    async.run(function()
+        local elapsed = 0
+        while true do
+            async.util.sleep(6000)
+            print(" 6 seconds has been passed")
+            elapsed = elapsed + 6
+
+            M.timeSpent = format_time(elapsed)
+
+            if M.stop == true then
+                break
+            end
+        end
+    end)
+end
+
+startTimer()
+end
+
+return M
